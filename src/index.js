d0424f884a255ded07945985290cfb3b041be476
jojo7necio <jlnecio2006@gmail.com>
2021-03-17 00:13:55 +0800

    final commit.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ function booleanToString(b){
         } else {
             return 'false';
         }
+    }else{
+        return '';
     }
 }
 
--- test/index.js
+++ test/index.js
@@ -3,17 +3,17 @@ let assert = require('assert');
 
 describe("A function which convert the given boolean value into its string representation.", function(){
     it('String is given.', function() {
-        let result = booleanToString('true');
-        assert.strictEqual(result, 'true');
+        let result = booleanToString('');
+        assert.strictEqual(result, '');
     });
     
     it('true is given.', function() {
         let result = booleanToString(true);
-        assert.strictEqual(result, 'true');
+        assert.strictEqual(result, "true");
     });
 
     it('false is given.', function() {
         let result = booleanToString(false);
-        assert.strictEqual(result, 'false');
+        assert.strictEqual(result, "false");
     });
 });
